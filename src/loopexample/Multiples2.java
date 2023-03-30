ee0ed8de58d1cdd449fcdf8205e0b312312ca35d
jobinjoseph2050 <jobinjoseph2050@gmail.com>
2023-03-30 07:15:28 -0400

    loop example

---
+++ src/loopexample/Multiples2.java
@@ -0,0 +1,21 @@
+package loopexample;
+
+public class Multiples2 {
+	
+		int number;
+		int counter;
+		
+		void printMultiples() {
+			
+			while(counter<=12) {
+				number = counter * 2;
+				System.out.println(number);
+				counter++;
+			
+			}
+		}
+	}
+
+
+
+
---
+++ src/loopexample/Multiples2main.java
@@ -0,0 +1,17 @@
+package loopexample;
+
+
+public class Multiples2main {
+
+	public static void main(String[] args) {
+	
+			
+			Multiples2 multiplesof2 = new Multiples2();
+			
+			multiplesof2.counter=1;
+			multiplesof2.printMultiples();
+
+		}
+
+	}
+
