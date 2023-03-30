8afad2379710840e70566ae7dcfee7d0db4f3162
jobinjoseph2050 <jobinjoseph2050@gmail.com>
2023-03-30 19:09:43 -0400

    First commit

---
+++ src/dowhileexample/First10num.java
@@ -0,0 +1,20 @@
+package dowhileexample;
+
+public class First10num {
+
+	int num;
+	
+	void first10num(){
+	
+		do
+		{
+			System.out.println(num);
+			num++;
+		}
+		while(num<=10);
+		
+	
+	}
+}
+
+
---
+++ src/dowhileexample/First10num_main.java
@@ -0,0 +1,13 @@
+package dowhileexample;
+
+public class First10num_main {
+
+	public static void main(String[] args) {
+		
+		First10num printfirst10num = new First10num();
+		printfirst10num.num=1;
+		printfirst10num.first10num();
+
+	}
+
+}
